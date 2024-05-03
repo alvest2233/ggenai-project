1505306a9a557635a7ca3b7f4bef873c948df89b
Tiago Alves <alvest2233@gmail.com>
2024-05-03 01:51:09 -0400

    source code viewer

--- ggenai-front/src/app/components/sitemessage.js
+++ ggenai-front/src/app/components/sitemessage.js
@@ -1,5 +1,30 @@
+import { useState } from 'react';
+import MonacoEditor from 'react-monaco-editor';
+
 export default function SiteMessage(contents) {
-    return (
+  const [showSource, setShowSource] = useState(false);
+
+  const handleClick = () => {
+    setShowSource(!showSource);
+  };
+
+  return (
+    <div style={{ width: '600px', height: '400px' }}>
+      <button onClick={handleClick}>
+        Toggle Source Code
+      </button>
+      {showSource ? (
+        <MonacoEditor
+          width="100%"
+          height="100%"
+          language="html"
+          theme="vs-dark"
+          value={contents.sourceCode}
+          options={{ readOnly: true }}
+        />
+      ) : (
         <iframe srcDoc={contents.sourceCode} width="100%" height="100%" />
-    );
-  }
+      )}
+    </div>
+  );
+}
