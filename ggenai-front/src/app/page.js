832f975d70024c0eda39701a59430cfd47515aae
lblommesteyn <lblommes@uwo.ca>
2024-05-03 02:47:25 -0400

    add all files

--- ggenai-front/src/app/page.js
+++ ggenai-front/src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
       <header className="bg-gray-800 p-4">
         <div className="container mx-auto flex items-center">
           <img src="https://www.w3schools.com/w3images/avatar2.png" alt="Logo" className="h-8 mr-2" />
-          <span className="text-white font-semibold text-lg">Chat App</span>
+          <span className="text-white font-semibold text-lg">Ai Wireframer App!</span>
         </div>
       </header>
       <div className="flex-1 bg-gray-100 flex ">
